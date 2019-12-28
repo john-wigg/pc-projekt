5c1e2715191ade3b7166a1449c27c35a37972e4c
John Wigg <linux@DESKTOP-HSPV433.localdomain>
2019-12-28 22:41:33 +0100

    Erste Implementierung des vollst. parallelen Algorithtmus

--- waermeleitung.c
+++ waermeleitung.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define T_LEFT 0
+#define T_TOP 1
+#define T_RIGHT 2
+#define T_BOTTOM 3
+
 // Initialisiere Wärmefeld mit Startwerten:
 // innen: 0.0
 // Rand:
@@ -187,18 +192,80 @@ int main(int argc, char **argv) {
   // Iteration im Block.
   // * Nach jedem Schritt wird der zu aktualisierende Block um 1 verkleinert.
   // * TODO: Nach g Schritten müssen die Randbereiche ausgetauscht werden.
-  // * TODO: Die globalen Ränder dürfen nicht mit aktulaisiert werden.
+  // * Die globalen Ränder dürfen nicht mit aktulaisiert werden.
+  MPI_Request req;
+  MPI_Status stat;
+  double *buf;
+  buf = (double *)malloc(g * bsize * sizeof(double));
   int l, k, i, j;
   for (l = 0; l < iter / g; l++) {
     // TODO: Randbereiche austauschen.
+    int n_left = bi * num_blocks_per_row + bj - 1;
+    int n_right = bi * num_blocks_per_row + bj + 1;
+    int n_top = (bi - 1) * num_blocks_per_row + bj;
+    int n_bot = (bi + 1) * num_blocks_per_row + bj;
+    if (bi > 0) {
+      // Oberen Rand nach oben senden und unteren Rand von Oben empfangen
+      MPI_Isend(u1 + g * bsize, g * bsize, MPI_DOUBLE, n_top, T_TOP,
+                MPI_COMM_WORLD, &req);
+      MPI_Recv(u1, g * bsize, MPI_DOUBLE, n_top, T_BOTTOM, MPI_COMM_WORLD,
+               &stat);
+    }
+    if (bi < num_blocks_per_col - 1) {
+      // Unteren Rand nach unten senden und oberen Rand von unten empfangen
+      MPI_Isend(u1 + bsize * (bsize - 2 * g), g * bsize, MPI_DOUBLE, n_bot,
+                T_BOTTOM, MPI_COMM_WORLD, &req);
+      MPI_Recv(u1 + bsize * (bsize - g), g * bsize, MPI_DOUBLE, n_bot, T_TOP,
+               MPI_COMM_WORLD, &stat);
+    }
+
+    if (bj > 0) {
+      // Linken Rand nach links senden und rechten Rand von links empfangen.
+      int m, n;
+      for (m = 0; m < bsize; m++) {
+        for (n = 0; n < g; n++) {
+          buf[m * g + n] = u1[m * bsize + g + n];
+        }
+      }
+      MPI_Isend(buf, g * bsize, MPI_DOUBLE, n_left, T_LEFT, MPI_COMM_WORLD,
+                &req);
+      MPI_Recv(buf, g * bsize, MPI_DOUBLE, n_left, T_RIGHT, MPI_COMM_WORLD,
+               &stat);
+      for (m = 0; m < bsize; m++) {
+        for (n = 0; n < g; n++) {
+          u1[m * bsize + n] = buf[m * g + n];
+        }
+      }
+    }
+
+    if (bj < num_blocks_per_row - 1) {
+      // Rechten Rand nach rechts senden und linken Rand von rechts
+      int m, n;
+      for (m = 0; m < bsize; m++) {
+        for (n = 0; n < g; n++) {
+          buf[m * g + n] = u1[m * bsize + bsize - 2 * g + n];
+        }
+      }
+      MPI_Isend(buf, g * bsize, MPI_DOUBLE, n_right, T_RIGHT, MPI_COMM_WORLD,
+                &req);
+      MPI_Recv(buf, g * bsize, MPI_DOUBLE, n_right, T_LEFT, MPI_COMM_WORLD,
+               &stat);
+
+      for (m = 0; m < bsize; m++) {
+        for (n = 0; n < g; n++) {
+          u1[m * bsize + bsize - g + n] = buf[m * g + n];
+        }
+      }
+    }
+
     for (k = 0; k < g; k++) {
       for (i = 1; i < bsize - 1 - k; i++) {
         for (j = 1; j < bsize - 1 - k; j++) {
           // Randbereiche mit Dicke g sollen nicht mit aktualisiert werden.
-          if (imin + i - g == 0 || imin + i - g == size - 1 ||
-              jmin + j - g == 0 || jmin + j - g == size - 1)
+          if (imin + i - g == 0 || imin + i - g >= size - 1 ||
+              jmin + j - g == 0 || jmin + j - g >= size - 1)
             continue;
-          updateCell(u1, u2, i, j, adjstep, bsize - k);
+          updateCell(u1, u2, i, j, adjstep, bsize);
         }
       }
       swap(&u1, &u2);
@@ -248,6 +315,13 @@ int main(int argc, char **argv) {
   sprintf(bfname, "out%d.ppm", rank);
   printResult(u1, bsize, bfname);
 
+  free(u1);
+  free(u2);
+
+  if (rank == 0) {
+    free(recvbuf);
+    free(u);
+  }
   MPI_Finalize();
   return 0;
 }
