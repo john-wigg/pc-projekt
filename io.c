0cc162814f50f571e20d1c9faed18c2fe7c8b6ec
CaptainProton42 <john.wigg@gmx.net>
2020-01-17 14:40:44 +0100

    Alte AUsgaberoutinen entfernt

--- io.c
+++ io.c
@@ -5,80 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printPPM(double *t, int size, const char *filename) {
-  FILE *f = fopen(filename, "w");
-  fprintf(f, "P3\n%i %i\n255\n", size, size);
-  double tmax = 25.0;
-  double tmin = -tmax;
-  double r, g, b;
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      double val = t[j + i * size];
-      r = 0;
-      g = 0;
-      b = 0;
-      if (val <= tmin) {
-        b = 1.0 * 255.0;
-      } else if (val >= -25.0 && val < -5) {
-        b = 255 * 1.0;
-        g = 255 * ((val + 25) / 20);
-      } else if (val >= -5 && val <= 0.0) {
-        g = 255 * 1.0;
-        b = 255 * (1.0 - (val + 5) / 5);
-      } else if (val > 0.0 && val <= 5) {
-        g = 255 * 1.0;
-        r = 255 * ((val) / 5);
-      } else if (val > 5 && val < 25.0) {
-        r = 255 * 1.0;
-        g = 255 * ((25 - val) / 20);
-      } else {
-        r = 255 * 1.0;
-      }
-      fprintf(f, "%i\n%i\n%i\n", (int)r, (int)g, (int)b);
-    }
-    //      fprintf(f,"\n");
-  }
-  fclose(f);
-}
-
-void printPPMP6(double *t, int size, const char *filename) {
-  FILE *f = fopen(filename, "w");
-  fprintf(f, "P6\n%i %i\n255\n", size, size);
-  double tmax = 25.0;
-  double tmin = -tmax;
-  double r, g, b;
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      double val = t[j + i * size];
-      r = 0;
-      g = 0;
-      b = 0;
-      if (val <= tmin) {
-        b = 1.0 * 255.0;
-      } else if (val >= -25.0 && val < -5) {
-        b = 255 * 1.0;
-        g = 255 * ((val + 25) / 20);
-      } else if (val >= -5 && val <= 0.0) {
-        g = 255 * 1.0;
-        b = 255 * (1.0 - (val + 5) / 5);
-      } else if (val > 0.0 && val <= 5) {
-        g = 255 * 1.0;
-        r = 255 * ((val) / 5);
-      } else if (val > 5 && val < 25.0) {
-        r = 255 * 1.0;
-        g = 255 * ((25 - val) / 20);
-      } else {
-        r = 255 * 1.0;
-      }
-      fprintf(f, "%c%c%c", (char)r, (char)g, (char)b);
-    }
-    //      fprintf(f,"\n");
-  }
-  fclose(f);
-}
-
-void printPPMP6MPI(double *t, int size, int bwidth, int bheight, int imin,
-                   int jmin, int g, const char *filename, int rank) {
+void printPPM(double *t, int size, int bwidth, int bheight, int imin, int jmin,
+              int g, const char *filename, int rank) {
   char header[128];
   sprintf(header, "P6\n%i %i\n255\n", size, size);
   int header_len = strlen(header);
